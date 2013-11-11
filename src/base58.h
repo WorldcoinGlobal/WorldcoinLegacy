a2b1da4934304e60581d82644d32a20f34ae5771
gudmunsn <gudmunsn@hotmail.com>
2013-11-11 03:07:08 -0600

    updating WDC with correct wallet starting letter

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Worldcoin addresses start with L
+        PUBKEY_ADDRESS = 73, // Worldcoin addresses start with W
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
