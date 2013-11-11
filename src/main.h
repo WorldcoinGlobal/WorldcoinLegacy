d59350a647ee0dcb164bd199caca3660343f4458
gudmunsn <gudmunsn@hotmail.com>
2013-11-11 04:06:41 -0600

    updating coin specs in main.h

--- src/main.h
+++ src/main.h
@@ -50,7 +50,7 @@ static const int64 DUST_SOFT_LIMIT = 1000000; // 0.01 WDC
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 10000;   // 0.0001 WDC mininput
 /** No amount larger than this (in satoshi) is valid */
-static const int64 MAX_MONEY = 84000000 * COIN;
+static const int64 MAX_MONEY = 280000000 * COIN;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
